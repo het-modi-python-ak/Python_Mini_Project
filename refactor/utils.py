13f1e5a5c99afd15da3462893c7b48f78a091065
Het Modi <het.modi@armakuni.com>
2026-02-13 14:10:14 +0530

    Delete refactor directory

--- refactor/utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def validate_date(date_text):
-    try:
-        date_obj = datetime.strptime(date_text, "%Y-%m-%d %H:%M")
-        return date_obj, None
-    except ValueError:
-        return None, "Invalid format! Use YYYY-MM-DD HH:MM"
